9d1bb9622dd97b9871e92d43a5bb310c3dc452b2
pheiselmann <prhmann@gmail.com>
2017-05-17 23:57:08 -0700

    full basic functionality

--- app.js
+++ app.js
@@ -44,35 +44,6 @@ function resetGame(state) {
   setRoute(state, 'start');
 };
 
-// Render functions
-function renderApp(state, elements) {
-  // default to hiding all routes, then show the current route
-  Object.keys(elements).forEach(function(route) {
-    elements[route].hide();
-  });
-  elements[state.route].show();
-
-  if (state.route === 'start') {
-      renderStartPage(state, elements[state.route]);
-  }
-  // else if (state.route === 'question') {
-  //     renderQuestionPage(state, elements[state.route]);
-  // }
-  // else if (state.route === 'answer-feedback') {
-  //   renderAnswerFeedbackPage(state, elements[state.route]);
-  // }
-  // else if (state.route === 'final-feedback') {
-  //   renderFinalFeedbackPage(state, elements[state.route]);
-  // }
-};
-
-// at the moment, `renderStartPage` doesn't do anything, because
-// the start page is preloaded in our HTML, but we've included
-// the function and used above in our routing system so that this
-// application view is accounted for in our system
-function renderStartPage(state, element) {
-};
-
 function createStateArrays(state, data) {
   if (data.Similar.Results) {
       state.results = data.Similar.Results;
@@ -167,7 +138,7 @@ function createStarArray(state) {
       for (var x = 0; x < asProbArray.length; x++) {
         if (asProbArray[x] === "as") {
             asTarget = x;
-            state.stars[i] = asProbArray.splice(0, asTarget).join('');
+            state.stars[i] = asProbArray.splice(0, asTarget).join(' ');
         }
       }
    }
@@ -193,6 +164,172 @@ function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+
+// function answerQuestion(state, answer) {
+//   var currentQuestion = state.questions[state.currentQuestionIndex];
+//   state.lastAnswerCorrect = currentQuestion.correctChoiceIndex === answer;
+//   if (state.lastAnswerCorrect) {
+//     state.score++;
+//   }
+//   selectFeedback(state);
+//   setRoute(state, 'answer-feedback');
+// };
+
+// Render functions
+function renderApp(state, elements) {
+  // default to hiding all routes, then show the current route
+  Object.keys(elements).forEach(function(route) {
+    elements[route].hide();
+  });
+  elements[state.route].show();
+
+  if (state.route === 'start') {
+      renderStartPage(state, elements[state.route]);
+  }
+  else if (state.route === 'genre') {
+      renderGenrePage(state, elements[state.route]);
+  }
+  else if (state.route === 'director') {
+      renderDirectorPage(state, elements[state.route]);
+  }
+  else if (state.route === 'star') {
+      renderStarPage(state, elements[state.route]);
+  }
+  else if (state.route === 'final') {
+      renderFinalPage(state, elements[state.route]);
+  }
+};
+
+// at the moment, `renderStartPage` doesn't do anything, because
+// the start page is preloaded in our HTML, but we've included
+// the function and used above in our routing system so that this
+// application view is accounted for in our system
+function renderStartPage(state, element) {
+};
+
+function renderGenrePage(state, element) {
+  //renderQuestionCount(state, element.find('.question-count'));
+  //renderQuestionText(state, element.find('.question-text'));
+  renderGenreChoices(state, element.find('.choices'));
+};
+
+// function renderQuestionText(state, element) {
+//   var currentQuestion = state.questions[state.currentQuestionIndex];
+//   element.text(currentQuestion.text);
+// };
+
+function renderGenreChoices(state, element) {
+  //var currentQuestion = state.questions[state.currentQuestionIndex];
+  var choices = state.genrePicks.map(function(choice, index) {
+    return (
+      '<li>' +
+        '<input type="radio" name="user-answer1" value="' + index + '" required>' +
+        '<label>' + choice + '</label>' +
+      '</li>'
+    );
+  });
+  element.html(choices);
+};
+
+function renderDirectorPage(state, element) {
+  //renderQuestionCount(state, element.find('.question-count'));
+  //renderQuestionText(state, element.find('.question-text'));
+  renderDirectorChoices(state, element.find('.choices'));
+};
+
+// function renderQuestionText(state, element) {
+//   var currentQuestion = state.questions[state.currentQuestionIndex];
+//   element.text(currentQuestion.text);
+// };
+
+function renderDirectorChoices(state, element) {
+  //var currentQuestion = state.questions[state.currentQuestionIndex];
+  var choices = state.directorPicks.map(function(choice, index) {
+    return (
+      '<li>' +
+        '<input type="radio" name="user-answer2" value="' + index + '" required>' +
+        '<label>' + choice + '</label>' +
+      '</li>'
+    );
+  });
+  element.html(choices);
+};
+
+function renderStarPage(state, element) {
+  //renderQuestionCount(state, element.find('.question-count'));
+  //renderQuestionText(state, element.find('.question-text'));
+  renderStarChoices(state, element.find('.choices'));
+};
+
+// function renderQuestionText(state, element) {
+//   var currentQuestion = state.questions[state.currentQuestionIndex];
+//   element.text(currentQuestion.text);
+// };
+
+function renderStarChoices(state, element) {
+  //var currentQuestion = state.questions[state.currentQuestionIndex];
+  var choices = state.starPicks.map(function(choice, index) {
+    return (
+      '<li>' +
+        '<input type="radio" name="user-answer3" value="' + index + '" required>' +
+        '<label>' + choice + '</label>' +
+      '</li>'
+    );
+  });
+  element.html(choices);
+};
+
+//Modify this to create links to three movies with choices as label
+function renderFinalPage(state, element) {
+  var text = "You chose " + state.movieKeys[0] + ", " +
+    state.movieKeys[1] + ", and " + state.movieKeys[2];
+  element.text(text);
+}
+
+
+// Event handlers
+
+
+$(".restart-game").click(function(event){
+  event.preventDefault();
+  resetGame(state);
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+
+$("form[name='genre-choices']").submit(function(event) {
+  event.preventDefault();
+  var answer1 = $("input[name='user-answer1']:checked").val();
+  state.movieKeys.push(state.genrePicks[answer1]);
+  alert(state.movieKeys);
+  //answer = parseInt(answer, 10);
+  //answerQuestion(state, answer);
+  setRoute(state, 'director');
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+$("form[name='director-choices']").submit(function(event) {
+  event.preventDefault();
+  var answer2 = $("input[name='user-answer2']:checked").val();
+  state.movieKeys.push(state.directorPicks[answer2]);
+  alert(state.movieKeys);
+  //answer = parseInt(answer, 10);
+  //answerQuestion(state, answer);
+  setRoute(state, 'star');
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+
+$("form[name='star-choices']").submit(function(event) {
+  event.preventDefault();
+  var answer3 = $("input[name='user-answer3']:checked").val();
+  state.movieKeys.push(state.starPicks[answer3]);
+  //answer = parseInt(answer, 10);
+  //answerQuestion(state, answer);
+  setRoute(state, 'final');
+  renderApp(state, PAGE_ELEMENTS);
+});
 
 
 
@@ -221,21 +358,30 @@ function displayTasteDiveSearchData(data) {
 }
 
 
-
-
 function watchSubmit(state) {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
     var query = $(this).find('.js-query').val();
     
-    getDataFromApi(query, displayTasteDiveSearchData);
+    //getDataFromApi(query, displayTasteDiveSearchData);
+    getDataFromApi(query, startProcess);
   });
 }
 
-// Event handlers
+function startProcess(data) {
+  createStateArrays(state, data);
+  setRoute(state, 'genre');
+  renderApp(state, PAGE_ELEMENTS);
+}
+
+
+
 var PAGE_ELEMENTS = {
-  'start': $('.start-page')
-  //,
+  'start': $('.start-page'),
+  'genre': $('.genre-page'),
+  'director': $('.director-page'),
+  'star': $('.star-page'),
+  'final': $('.final-page')
   // 'question': $('.question-page'),
   // 'answer-feedback': $('.answer-feedback-page'),
   // 'final-feedback': $('.final-feedback-page')
